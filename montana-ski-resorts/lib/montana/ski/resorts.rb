52053c9d3e9b6ce485c85461848e321575aef8cc
Kate Preston <katejsenger@yahoo.com>
2018-12-06 20:48:17 +0000

    Automatically backed up by Learn

---
+++ montana-ski-resorts/lib/montana/ski/resorts.rb
@@ -0,0 +1,10 @@
+require "montana/ski/resorts/version"
+
+module Montana
+  module Ski
+    module Resorts
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
